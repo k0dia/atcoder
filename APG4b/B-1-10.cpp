bfcb121d0441b13fcecf1efc9fd1d586e1b6e2ad
k0dia <wifi.tky.18@gmail.com>
2020-02-04 00:44:29 +0900

    b10ミス

---
+++ APG4b/B-1-10.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A, B;
+  cin >> A >> B;
+  int i = 0, j = 0;
+ 
+  // ここにプログラムを追記
+  while(i < A){
+      cout << "]";
+      i++;
+  }
+  cout << endl;
+
+  while(j < B){
+      cout << "]";
+      j++;
+  }
+  cout << endl;
+}
