0dc856daa23fd879b1c5e10f14b3e03bb0c3d430
k0dia <wifi.tky.18@gmail.com>
2020-02-05 00:04:04 +0900

    charはシングルクォーテーション stringはダブルクォーテーション AC

---
+++ APG4b/B-1-12.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  string S;
+  cin >> S;
+ 
+  // ここにプログラムを追記
+  int sum = 1;
+
+  for (int i = 0; i < S.size();i++){
+      if (S.at(i) == '+'){
+          sum++;
+      }else if (S.at(i) == '-'){
+          sum--;
+      }
+  }
+
+  cout << sum << endl;
+}
